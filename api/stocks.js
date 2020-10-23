3e25c1ec853969cf6b1b45e1be0067a6ff558b29
unknown <nestore@DESKTOP-661C9BH.Edisac.local>
2020-10-23 14:31:02 +0200

    add product schema

---
+++ api/stocks.js
@@ -0,0 +1,7 @@
+const {axios_prestashop} = require('../services/axiosPrestashop')
+const builder = require('xmlbuilder2');
+
+const headerXml =  {
+    headers: {'Content-Type': 'application/xml'}
+};
+
